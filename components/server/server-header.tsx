2726952bdf1418c0dcb8a93b2f12ed7d339de53f
BONX <winhackger@gmail.com>
2023-11-09 18:35:18 +0700

    feat: Server Settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,7 +50,10 @@ const ServerHeader = ({ server, role }: ServerHaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className=" px-4 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className=" px-4 py-2 text-sm cursor-pointer"
+          >
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
